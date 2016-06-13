e5db269aaae5a134eac09af99bc5f52cc1fa0ba4
Bill Adona <bill.adona08@gmail.com>
2016-06-12 19:15:33 -0600

    second push
    some CSS added to build the GUI of the front end

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { main_title: "Bill's summer project" });
+  res.render('index', { main_title: "Bill I Am's Supermarket" });
 });
 
 module.exports = router;
